5d1b389b902a5f14e076aef7cda71777d958fbe1
Sam170703 <accesopuerta0@gmail.com>
2021-12-31 17:42:31 +0100

    confirmar en "master"

--- commands/servidores.js
+++ commands/servidores.js
@@ -5,7 +5,7 @@ module.exports = {
     run: async (client, message, args) => {
         let ID = ["911352430963347537"]
 
-  if(!ID.includes(message.author.id)) return message.channel.send("**<:Warn:923949755665641502>| Error :** No tienes permisos")//.then(msg => msg.delete({timeout: 9000}));
+  if(!ID.includes(message.author.id)) return message.channel.send("**<:aviso:925769011822727209>| Error :** No tienes permisos")//.then(msg => msg.delete({timeout: 9000}));
 ;
   message.channel.send("**_///////////_ Servidores _///////////_**")
   message.channel.send(`${client.guilds.cache.map(g => g.name).join('\n')}`)
