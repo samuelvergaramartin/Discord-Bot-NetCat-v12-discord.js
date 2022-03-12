aaecc0ed6eca476d27c4363d7b671f606238e61d
Samuel.V <91561846+Sam170703@users.noreply.github.com>
2022-03-12 18:42:06 +0100

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -3,7 +3,7 @@ const Discord = require ('discord.js');
 module.exports = {
     name: "test",
     run: async (client, message, args) => {
-        message.channel.send('Todo en orden')
+        message.channel.send('Todo en orden.')
         
 
        
